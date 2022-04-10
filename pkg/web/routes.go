76e59aa3eee1db8d3785e6862301d8696d11649d
pavel1337 <pavelvoronov1337@gmail.com>
2022-04-10 18:39:14 +0200

    Make session storage configurable

--- pkg/web/routes.go
+++ pkg/web/routes.go
@@ -9,15 +9,14 @@ import (
 
 func (app *Web) routes() http.Handler {
 	standartMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-	dynamicMiddleware := alice.New(app.session.Enable)
 
 	mux := pat.New()
 	mux.Get("/ping", http.HandlerFunc(app.ping))
-	mux.Get("/", dynamicMiddleware.ThenFunc(app.createSecretForm))
-	mux.Get("/secret/create", dynamicMiddleware.ThenFunc(app.createSecretForm))
-	mux.Post("/secret/create", dynamicMiddleware.ThenFunc(app.createSecret))
-	mux.Get("/secret/:id", dynamicMiddleware.ThenFunc(app.showSecretForm))
-	mux.Post("/secret/:id", dynamicMiddleware.ThenFunc(app.showSecret))
+	mux.Get("/", http.HandlerFunc((app.createSecretForm)))
+	mux.Get("/secret/create", http.HandlerFunc((app.createSecretForm)))
+	mux.Post("/secret/create", http.HandlerFunc((app.createSecret)))
+	mux.Get("/secret/:id", http.HandlerFunc((app.showSecretForm)))
+	mux.Post("/secret/:id", http.HandlerFunc((app.showSecret)))
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 
